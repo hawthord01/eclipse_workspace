f669f74774bf7ea948906f7d72a0d6dcd037503f
Daylen Hawthorne <hawthord@student.psdr3.org>
2018-01-04 13:03:52 -0600

    Person and PersonTester
    Person and PersonTester class added

---
+++ Chapter11/src/PersonTester.java
@@ -0,0 +1,15 @@
+
+public class PersonTester {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.setAge(12);
+		p1.setName("Dylan");
+		p1.toString();
+		Person p2 = new Person();
+		p2.setAge(15);
+		p2.setName("Angel");
+		p2.toString();
+	}
+
+}
